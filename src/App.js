8adfa3e3b78f57f2f027292d1305bc972c3fe692
khanglt <khanglamthe@gmail.com>
2022-12-27 11:20:12 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Clothes from './Clothes'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Clothes name="Quần jean" type="Skinny" color ="Đen" size = "L">Clothes 1</Clothes>
+      <Clothes name="Váy" type="váy công chúa" color ="Trắng" size = "M">Clothes 2</Clothes>
     </div>
   );
 }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelCome = function (props){
+    return (
+        <div>
+            <h1>Welcome ! I am a functional component </h1>
+        </div>
+    )
+}
+
+export default WelCome;
